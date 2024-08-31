0fb0c0a25c061ceef5f8e18dac8b583585d33c31
amaribattle1 <124077929+amaribattle1@users.noreply.github.com>
2024-08-31 15:10:05 -0400

    first commit

---
+++ amplify/auth/resource.ts
@@ -0,0 +1,16 @@
+import { defineAuth } from "@aws-amplify/backend";
+
+
+export const auth = defineAuth({
+  loginWith: {
+    email: {
+      verificationEmailStyle: "CODE",
+      verificationEmailSubject: "Welcome to the ExpenseTracker!",
+      verificationEmailBody: (createCode) =>
+        `Use this code to confirm your account: ${createCode()}`,
+    },
+  },
+});
+
+
+
