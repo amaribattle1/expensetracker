f2351e77354a4ba7bcc289cbcd48ca1517d31c85
amaribattle1 <124077929+amaribattle1@users.noreply.github.com>
2024-09-06 14:14:53 -0400

    first commit

--- amplify/auth/resource.ts
+++ amplify/auth/resource.ts
@@ -1,6 +1,5 @@
 import { defineAuth } from "@aws-amplify/backend";
 
-
 export const auth = defineAuth({
   loginWith: {
     email: {
@@ -11,6 +10,5 @@ export const auth = defineAuth({
     },
   },
 });
-
 
 
